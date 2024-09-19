eac62a4d26201a715bb47e1c6d809bff309e68ac
Man-Sinh-Lee <man.sinh.lee@outlook.com>
2024-09-20 06:28:37 +0700

    update

--- ch05/5-1-example.py
+++ ch05/5-1-example.py
@@ -2,8 +2,9 @@ import http.server
 from prometheus_client import start_http_server, Counter
 
 REQUESTS = Counter('hello_worlds_total',
-        'Hello Worlds requested.',
-        labelnames=['path'])
+                   'Hello Worlds requested.',
+                   labelnames=['path'])
+
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -12,7 +13,8 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Hello World")
 
+
 if __name__ == "__main__":
     start_http_server(8000)
-    server = http.server.HTTPServer(('localhost', 8001), MyHandler)
+    server = http.server.HTTPServer(('192.168.1.111', 8090), MyHandler)
     server.serve_forever()
